302e2257e80331904cd5e5cf31fa545005333906
Yash Nagtode <nagtodeyash123@gmail.com>
2025-02-09 15:44:43 +0530

    changes - profile, edit, favorites

--- TMS_Native_app/App.js
+++
@@ -1,9 +0,0 @@
-DMC_Sunbeam / FInalProject / NativeApp / TMSapp / App.js
-import React from 'react';
-import Launcher from './screens/Launcher';
-
-function App() {
-  return <Launcher />;
-}
-
-export default App;
